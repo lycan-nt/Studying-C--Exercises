539d82e93f01513b4869ceb965a03a6b357ce02c
Felipe D. santos <lycan.felipe@gmail.com>
2020-02-27 06:13:06 -0300

    Hellow World

---
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is comment
+
+            string name = "Felipe D. Santos";
+          
+            Console.WriteLine("Hello World! " + name);
+
+            int myNum = 10;
+            myNum = 100; //myNum is now 100
+            Console.WriteLine("Nota: " + myNum);
+
+            int x = 5;
+            int y = 5;
+            Console.WriteLine("Soma: X + Y = " + ( x + y)); // Print the value of x + y 
+        }
+    }
+}
