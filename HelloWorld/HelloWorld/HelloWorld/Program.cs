0201610a020a60b88ea6a20293692654bfe22667
Felipe D. santos <lycan.felipe@gmail.com>
2020-03-12 05:16:35 -0300

    While and for loops

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
             //This is comment
 
             string name = "Felipe D. Santos";
-          
+
             Console.WriteLine("Hello World! " + name);
 
             int myNum = 10;
@@ -18,7 +18,7 @@ namespace HelloWorld
 
             int x = 5;
             int y = 5;
-            Console.WriteLine("Soma: X + Y = " + ( x + y)); // Print the value of x + y 
+            Console.WriteLine("Soma: X + Y = " + (x + y)); // Print the value of x + y 
 
             //Conversão implicita
 
@@ -61,20 +61,20 @@ namespace HelloWorld
 
             //if and else...
 
-            if(20 > 18)
+            if (20 > 18)
             {
                 Console.WriteLine("20 is greater than 18");
             }
 
             int ifNum1 = 20;
             int ifNum2 = 10;
-            if(ifNum1 > ifNum2)
+            if (ifNum1 > ifNum2)
             {
                 Console.WriteLine("ifNum1 is greater than ifNum2");
             }
 
             int time = 20;
-            if(time < 18)
+            if (time < 18)
             {
                 Console.WriteLine("God Day!");
             }
@@ -85,6 +85,52 @@ namespace HelloWorld
 
             string result = (time > 18) ? "God nigth" : "God day!";
             Console.WriteLine(result);
+
+            //Switch
+
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Moonday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+
+            //While Loop
+
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int i2 = 1;
+            do
+            {
+                Console.WriteLine(i2);
+            } while (i2 > 1);
+
         }
+
     }
 }
