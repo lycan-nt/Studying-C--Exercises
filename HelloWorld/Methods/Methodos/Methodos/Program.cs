6f13198f854eb9f5d88e7743539dde71f6f23df5
Felipe D. santos <lycan.felipe@gmail.com>
2020-03-26 07:11:21 -0300

    Methods and params

---
+++ HelloWorld/Methods/Methodos/Methodos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Methodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+class Program
+{
+    static void MyMethod()
+    {
+        //Code
+    }
+
+
+
+}
+
+
+
+//Outputs "I just got executad"
+
+//=========================================================================================================
+ 
+//Parametros
+
+
+class Program2
+{
+    static void MyMethod(string fname)
+    {
+        Console.WriteLine("fname" + "D. Santos");
+    }
+
+    static void Main(string[] args)
+    {
+        MyMethod("Felipe");
+        MyMethod("Marcella");
+        MyMethod("Yuno");
+        MyMethod("Floop");
+    }
+
+}
+
