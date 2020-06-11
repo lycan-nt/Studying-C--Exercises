ca4e01e689441a61f9a7dadd6b2b0080ee9f7ce0
Felipe D. santos <lycan.felipe@gmail.com>
2020-06-11 05:39:15 -0300

    Refatorando

---
+++ C#_Completo/consoleApp/ConsoleApp/Funcoes/Tabuada.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Funcoes
+{
+    class Tabuada
+    {
+        public static void Calcular(int numero)
+        {
+            Console.WriteLine("======== Calculo da tabuada do " + numero + "\n==========");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " X " + i + " = " + (numero * i));
+            }
+            Console.WriteLine("==================");
+        }
+    }
+}
--- C#_Completo/consoleApp/ConsoleApp/Program.cs
+++ C#_Completo/consoleApp/ConsoleApp/Program.cs
@@ -1,116 +1,20 @@
-﻿using Calculo;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tela;
 
 namespace ConsoleApp
 {
 
     class Program
     {
-        private static int CalcularSoma()
-        {
-            int a = 1;
-            int b = 2;
-            int c = a + b;
-            return c;
-        }
-
-        public static void MostrarMesagemNaTela()
-        {
-            Console.WriteLine("Olá pessoal");
-        }
-
-        public static void Tabuada(int numero)
-        {
-            Console.WriteLine("======== Calculo da tabuada do " + numero + "\n==========");
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine(numero + " X " + i + " = " + (numero * i));
-            }
-            Console.WriteLine("==================");
-        }
-
-        private static void LerArquivos(int numeroArquivo)
-        {
-            string arquivoComCaminho = @"C:\arquivos\arq" + numeroArquivo + ".txt";
-            Console.WriteLine("==== Lendo arquivo ====\n" + arquivoComCaminho + "\n=====");
-            if (File.Exists(arquivoComCaminho))
-            {
-                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
-                {
-                    string linha;
-                    while ((linha = arquivo.ReadLine()) != null)
-                    {
-                        Console.WriteLine(linha);
-                    }
-                }
-            }
-            string arquivoComCaminho2 = @"C:\arquivos\arq" + (numeroArquivo + 1) + ".txt";
-            if (File.Exists(arquivoComCaminho2))
-            {
-                LerArquivos(numeroArquivo + 1);
-            }
-        }
-
-        
-        private static void Menu()
-        {
-            while (true)
-            {
-                string mensagem = "Olá usuário, bem vindo ao programa\n" +
-                    "\n  Utilizando programação funcional" +
-                    "\n\n" +
-                    "\n    Digite uma das opções abaixo:" +
-                    "\n      0 - Sair do programa" +
-                    "\n      1 - Para Ler Arquivos" +
-                    "\n      2 - Para executar a tabuada" +
-                    "\n      3 - Calcular média de alunos";
-                Console.WriteLine(mensagem);
-
-                int valor = int.Parse(Console.ReadLine());
-                if (valor == SAIDA_PROGRAMA)
-                {
-                    break;
-                }
-                else if (valor == LER_ARQUIVOS)
-                {
-                    Console.WriteLine("==== Opção Ler Arquivos ====");
-                    LerArquivos(1);
-                    Console.WriteLine("\n============================\n");
-                }
-                else if (valor == TABUADA)
-                {
-                    Console.WriteLine("==== Opção tabuada ====");
-                    Console.WriteLine("Digite o número que deseja na tabuada");
-                    int numero = int.Parse(Console.ReadLine());
-                    Tabuada(numero);
-                    Console.WriteLine("\n============================\n");
-                }
-                else if (valor == CALCULO_MEDIA)
-                {
-                    Media.Aluno();
-                                        
-                    Console.WriteLine("\n============================\n");
-                }
-                else
-                {
-                    Console.WriteLine("Opção inválida, digite novamente");
-                }
-            }
-        }
-
-        public const int SAIDA_PROGRAMA = 0;
-        public const int LER_ARQUIVOS = 1;
-        public const int TABUADA = 2;
-        public const int CALCULO_MEDIA = 3;
 
         static void Main(string[] args)
         {
-            Menu();
+            Menu.Criar();
         }
     }
 }
