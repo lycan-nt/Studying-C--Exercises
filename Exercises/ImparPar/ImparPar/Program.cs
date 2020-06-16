545b24c1ee917f2064fade04b6a6219a733c4ba2
Felipe D. santos <lycan.felipe@gmail.com>
2020-06-16 05:00:19 -0300

    Init ImparPar

---
+++ Exercises/ImparPar/ImparPar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImparPar
+{
+    class Program
+    {
+
+        public static int ImparPar(int number)
+        {
+            Console.WriteLine(" ");
+            int result = 0;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================Impar or Par APP=================!");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Please enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            
+        }
+    }
+}
