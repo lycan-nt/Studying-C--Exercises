fe6b2f8c1ba35e0aded4442544bcd34c0ddd80d1
Felipe D. santos <lycan.felipe@gmail.com>
2020-07-01 08:46:47 -0300

    Enter your name APP

---
+++ Exercises/EnterName/EnterName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EnterName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==================================Enter Name======================================");
+            Console.WriteLine("Whats your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("You name is: " + name);
+        }
+    }
+}
